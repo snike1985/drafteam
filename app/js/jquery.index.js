52547fb39161a9d2cc517f69126151a21e6658c1
alexserga <snike1985@gmail.com>
2018-05-01 09:50:41 +0300

    loaded project

---
+++ app/js/jquery.index.js
@@ -0,0 +1,15 @@
+( function(){
+
+    $( function(){
+
+        $( '.sidebar__wrap' ).niceScroll({
+            cursorcolor: 'rgb(255, 255, 255)',
+            cursoropacitymin: 0,
+            cursorwidth: '6px',
+            cursorborderradius: '3px',
+            cursorborder: '0'
+        });
+
+    });
+
+} )();
